b6ef0118ca86fa99b86a416d83eebe5767e618d4
maria <mariamibrahimmi4142726@gmail.com>
2024-07-26 00:35:33 +0300

    add app textstyles

---
+++ lib/core/utils/app_text_style.dart
@@ -0,0 +1,36 @@
+import 'package:chef_app/core/utils/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle _textStyle(
+    {required Color color,
+    required double fontSize,
+    required FontWeight fontWeight}) {
+  return GoogleFonts.lato(
+    color: color,
+    fontSize: fontSize,
+    fontWeight: fontWeight,
+  );
+}
+
+TextStyle boldStyle({
+  Color color = AppColors.black,
+  double fontSize = 24,
+}) {
+  return GoogleFonts.lato(
+    color: color,
+    fontSize: fontSize,
+    fontWeight: FontWeight.bold,
+  );
+}
+
+TextStyle regularStyle({
+  Color color = AppColors.black,
+  double fontSize = 24,
+}) {
+  return GoogleFonts.lato(
+    color: color,
+    fontSize: fontSize,
+    fontWeight: FontWeight.normal,
+  );
+}
