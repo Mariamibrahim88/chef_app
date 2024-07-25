7bb6a984e60569fe7b56cbb4dbb03982f8d62a96
maria <mariamibrahimmi4142726@gmail.com>
2024-07-25 23:05:48 +0300

    sdd appcolors and assets

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color Basecolor = Color(0xffFA9A0C);
+  static const Color black = Color(0xff111111);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color grey = Color(0xff9D9FA0);
+  static const Color orange = Color(0xffFB5607);
+}
