d6824cef6b790a18f177f5e90be1158039793d89
maria <mariamibrahimmi4142726@gmail.com>
2024-07-27 20:45:58 +0300

    app theme

---
+++ lib/core/themes/app_theme.dart
@@ -0,0 +1,40 @@
+import 'package:chef_app/core/utils/app_colors.dart';
+import 'package:chef_app/core/utils/app_text_style.dart';
+import 'package:flutter/material.dart';
+
+ThemeData getAppThem() {
+  return ThemeData(
+      scaffoldBackgroundColor: Colors.white,
+      primaryColor: AppColors.Basecolor,
+      appBarTheme: const AppBarTheme(
+        backgroundColor: AppColors.Basecolor,
+        elevation: 0,
+        // iconTheme: IconThemeData(
+        //   color: AppColors.black,
+        // ),
+      ),
+      textTheme:
+          TextTheme(displayLarge: boldStyle(), displayMedium: regularStyle()),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: AppColors.Basecolor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+        ),
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          // borderSide: BorderSide(
+          //   color: AppColors.grey,
+          // ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          // borderSide: BorderSide(
+          //   color: AppColors.Basecolor,
+          // ),
+        ),
+      ));
+}
