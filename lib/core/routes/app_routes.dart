7deaa34a2b7d45e0803593012857d30b0157a636
maria <mariamibrahimmi4142726@gmail.com>
2024-07-27 22:53:31 +0300

    app routes

---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,34 @@
+import 'package:chef_app/features/auth/presentation/views/change_lang_view.dart';
+import 'package:chef_app/features/auth/presentation/views/login_view.dart';
+import 'package:chef_app/features/auth/presentation/views/reset_password_view.dart';
+import 'package:chef_app/features/auth/presentation/views/send_code_view.dart';
+import 'package:chef_app/features/splash/presentation/views/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+class Routes {
+  static const String initialRoute = '/';
+  static const String login = '/login';
+  static const String changLan = '/changLan';
+  static const String sendCode = '/sendCode';
+  static const String resetPassw = '/resetPassw';
+}
+
+class AppRoutes {
+  static Route? generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.initialRoute:
+        return MaterialPageRoute(builder: (_) => const SplashScreenView());
+      case Routes.login:
+        return MaterialPageRoute(builder: (_) => const LoginView());
+      case Routes.changLan:
+        return MaterialPageRoute(builder: (_) => const ChangeLangScreen());
+      case Routes.sendCode:
+        return MaterialPageRoute(builder: (_) => const SendCodeView());
+      case Routes.resetPassw:
+        return MaterialPageRoute(builder: (_) => const ResetPasswordView());
+
+      default:
+        return MaterialPageRoute(builder: (_) => const Scaffold());
+    }
+  }
+}
---
+++ lib/features/auth/presentation/views/send_code_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SendCodeView extends StatelessWidget {
+  const SendCodeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
